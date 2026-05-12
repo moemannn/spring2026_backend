df22d5f448a1f5650a4bb5dc5d574001daa0f272
Ricardo <ricardobettonvil@gmail.com>
2026-05-12 16:41:14 +0200

    refactor entity/model from auto generated entity

--- src/entity/message_mapping/model.rs
+++ src/entity/message_mapping/model.rs
@@ -4,15 +4,23 @@ use super::r#type::Type;
 #[derive(Clone, Debug, PartialEq, DeriveEntityModel)]
 #[sea_orm(table_name = "message_mapping")]
 pub struct Model {
-    #[sea_orm(primary_key)]
-    pub id: i64,
-
-    pub target_id: Option<i64>,
-    pub scope_id: Option<i64>,
-    pub message_type: Type,
+    #[sea_orm(primary_key, auto_increment = false)]
+    pub id: Uuid,
+    pub message_type: String,
+    pub target_id: Uuid,
+    pub scope_id: Option<Uuid>,
 }
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
-pub enum Relation {}
+pub enum Relation {
+    #[sea_orm(has_many = "super::messages::Entity")]
+    Messages,
+}
+
+impl Related<super::messages::Entity> for Entity {
+    fn to() -> RelationDef {
+        Relation::Messages.def()
+    }
+}
 
 impl ActiveModelBehavior for ActiveModel {}
