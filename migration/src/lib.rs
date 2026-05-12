9c954641b18d2c5e844aafd3e042bd881a248cb0
Ricardo <ricardobettonvil@gmail.com>
2026-05-12 18:35:17 +0200

    changed migrations naming convections

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,13 +1,13 @@
 pub use sea_orm_migration::prelude::*;
 
-mod m20260301_000011_create_users_table;
-mod m20260301_000012_create_servers_table;
-mod m20260301_000013_create_channels_table;
-mod m20260301_000014_create_message_mapping_table;
-mod m20260301_000015_create_message_linking_table;
-mod m20260301_000016_create_messages_table;
-mod m20260301_000023_create_refresh_tokens_table;
-mod m20260301_000024_create_users_to_servers_table;
+mod m000011_create_users_table;
+mod m000012_create_servers_table;
+mod m000013_create_channels_table;
+mod m000014_create_message_mapping_table;
+mod m000015_create_message_linking_table;
+mod m000016_create_messages_table;
+mod m000023_create_refresh_tokens_table;
+mod m000024_create_users_to_servers_table;
 
 pub struct Migrator;
 
@@ -15,14 +15,14 @@ pub struct Migrator;
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
-            Box::new(m20260301_000011_create_users_table::Migration),
-            Box::new(m20260301_000012_create_servers_table::Migration),
-            Box::new(m20260301_000013_create_channels_table::Migration),
-            Box::new(m20260301_000014_create_message_mapping_table::Migration),
-            Box::new(m20260301_000015_create_message_linking_table::Migration),
-            Box::new(m20260301_000016_create_messages_table::Migration),
-            Box::new(m20260301_000023_create_refresh_tokens_table::Migration),
-            Box::new(m20260301_000024_create_users_to_servers_table::Migration),
+            Box::new(m000011_create_users_table::Migration),
+            Box::new(m000012_create_servers_table::Migration),
+            Box::new(m000013_create_channels_table::Migration),
+            Box::new(m000014_create_message_mapping_table::Migration),
+            Box::new(m000015_create_message_linking_table::Migration),
+            Box::new(m000016_create_messages_table::Migration),
+            Box::new(m000023_create_refresh_tokens_table::Migration),
+            Box::new(m000024_create_users_to_servers_table::Migration),
         ]
     }
 }
