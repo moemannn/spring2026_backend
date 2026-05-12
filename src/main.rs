d1ddb122015fe2a197913c1d03a6704500153be9
Ricardo <ricardobettonvil@gmail.com>
2026-05-12 21:23:35 +0200

    Added middleware cargo and implamented: RequestId, Logging

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,16 @@ mod entity;
 mod models;
 mod services;
 mod error;
+mod middleware;
 
 use db::connect_db;
 use std::sync::Arc;
 use axum::Router;
+use tower::ServiceBuilder;
+use tower_http::request_id::{MakeRequestUuid, SetRequestIdLayer, PropagateRequestIdLayer};
+use tower_http::trace::TraceLayer;
+
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[derive(Clone)]
 pub struct AppState {
@@ -19,13 +25,23 @@ pub struct AppState {
 async fn main() {
     dotenvy::dotenv().ok();
 
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .with(tracing_subscriber::EnvFilter::new("info"))
+        .init();
+
     let db = connect_db().await;
 
     let state = Arc::new(AppState { db });
-
     let app = Router::new()
         .merge(routes::default::get_default_routes(state.clone()))
-        .merge(routes::swagger::get_swagger_routes());
+        .merge(routes::swagger::get_swagger_routes())
+        .layer(
+            ServiceBuilder::new()
+                .layer(SetRequestIdLayer::x_request_id(MakeRequestUuid))
+                .layer(PropagateRequestIdLayer::x_request_id())
+                .layer(TraceLayer::new_for_http())
+        );
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
